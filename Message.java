cfb392e802f9df37b6e9f102a5e3bc08e2face32
Shambhunadager <Shambhunadagerxworkz.com>
2022-10-17 22:42:55 +0530

    first push

---
+++ Message.java
@@ -0,0 +1,20 @@
+class Message{
+
+static void PrintHello(){
+System.out.println("Hello");
+
+}
+static void Print(){
+PrintHello();
+System.out.println("Hi");
+
+}
+
+public static void main(String[] args){
+Print();
+Print();
+
+}
+
+}
+
