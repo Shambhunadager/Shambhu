4c52c6c36c084677bfcf901a8b94696e8872f42f
Shambhunadager <Shambhunadagerxworkz.com>
2022-12-02 09:36:19 +0530

    first push

--- Collection_java/ListCollection/DoctorDemo.java
+++ Collection_java/ListCollection/DoctorDemo.java
@@ -5,25 +5,25 @@ import java.util.ArrayList;
 public class DoctorDemo {
 	public static void main(String[] args) {
 
-		ArrayList<Doctor> patients = new ArrayList<Doctor>();
+		ArrayList<Doctor> doctors = new ArrayList<Doctor>();
 		Doctor sunil = new Doctor("sunil", "p01", "Pediatrition");
 		Doctor kiran = new Doctor("kiran", "p02", "Cardiologist");
 		Doctor punith = new Doctor("punith", "p03", "Nuerologist");
 		Doctor manju = new Doctor("manju", "p04", "Gynacologist");
 		Doctor shridhar = new Doctor("shridhar", "p05", "Physcian");
 
-		patients.add(sunil);
-		patients.add(kiran);
-		patients.add(shridhar);
-		patients.add(manju);
-		patients.add(punith);
+		doctors.add(sunil);
+		doctors.add(kiran);
+		doctors.add(shridhar);
+		doctors.add(manju);
+		doctors.add(punith);
 
-		for (Doctor d : patients) {
+		for (Doctor d : doctors) {
 			if (d.equals(shridhar)) {
 				System.out.println(d);
 			}
 		}
 
-		System.out.println(patients.contains(kiran));
+		System.out.println(doctors.contains(kiran));
 	}
 }
